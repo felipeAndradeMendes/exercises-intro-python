422804be6806b8b5092dc0962021baa426e282cf
Felipe Mendes <felipemadruga@gmail.com>
2023-08-15 19:10:31 -0300

    Conclui requisito 02

--- src/average.py
+++ src/average.py
@@ -2,4 +2,7 @@ from typing import List
 
 
 def find_average(numbers: List[int]) -> float:
-    raise NotImplementedError
+    # raise NotImplementedError
+    numbers_sum = sum(numbers)
+    divisor = len(numbers)
+    return numbers_sum / divisor
