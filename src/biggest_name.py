cbf0aa6172491dd07df0d63a7c2c15c592483d3d
Felipe Mendes <felipemadruga@gmail.com>
2023-08-15 19:19:34 -0300

    Conclui requisito 03

--- src/biggest_name.py
+++ src/biggest_name.py
@@ -2,4 +2,9 @@ from typing import List
 
 
 def find_biggest_name(names: List[str]) -> str:
-    raise NotImplementedError
+    # raise NotImplementedError
+    bigger_name = ''
+    for name in names:
+        if len(name) > len(bigger_name):
+            bigger_name = name
+    return bigger_name
